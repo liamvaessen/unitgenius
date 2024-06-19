77a72ffbd6acb4e00fbd6b045063130911ae1ff7
Liam Vaessen <liamvaessen@gmail.com>
2024-06-19 14:42:23 +0200

    Setup AKS cluster

--- infrastructure/scripts/setup-delete.sh
+++ infrastructure/scripts/setup-delete.sh
@@ -1,63 +1,57 @@
 #!/bin/bash
-#---------- Login to Azure ---------- 
-# Get tenantId, subscriptionId and appId from preferences.env
-source preferences.env
-echo -e "\n\n[UnitGenius Setup Tool]: Stored preference for tenantId: $tenantId"
-echo -e "\n[UnitGenius Setup Tool]: Stored preference for subscriptionId: $subscriptionId"
-echo -e "\n[UnitGenius Setup Tool]: Stored preference for appId: $appId"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
 
-echo -e "\n\n[UnitGenius Setup Tool]: Login to Azure"
+#---------- Login to Azure ---------- 
+source preferences.env
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Stored preference for tenantId: $tenantId"
+echo -e "\n${PURPLE}[UnitGenius Setup Tool]:${NC} Stored preference for subscriptionId: $subscriptionId"
+echo -e "\n${PURPLE}[UnitGenius Setup Tool]:${NC} Stored preference for appId: $appId"
+
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${YELLOW}Loging in to Azure...${NC}"
 login_output=$(az login)
 tenantId=$(echo $login_output | jq -r '.[0].tenantId')
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}Logged in to Azure...${NC}"
 
-echo "tenantId=$tenantId" >> preferences.env
-echo -e "\n\n[UnitGenius Setup Tool]: Stored preference for tenantId: $tenantId"
-
-echo -e "\n\n[UnitGenius Setup Tool]: Your Azure subscriptions: "
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Your Azure subscriptions: "
 az account list --output table
 
-echo -e "\n[UnitGenius Setup Tool]: Enter your preferred subscriptionId: "
+echo -e "\n${PURPLE}[UnitGenius Setup Tool]:${NC} Enter your preferred tenantId: "
+read tenantId
+
+echo "tenantId=$tenantId" > preferences.env
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Stored preference for tenantId: $tenantId"
+
+echo -e "\n${PURPLE}[UnitGenius Setup Tool]:${NC} Enter your preferred subscriptionId: "
 read subscriptionId
 
 az account set --subscription $subscriptionId
-echo -e "\n\n[UnitGenius Setup Tool]: Set subscription to $subscriptionId"
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Set subscription to $subscriptionId"
 
 echo "subscriptionId=$subscriptionId" >> preferences.env
-echo -e "\n\n[UnitGenius Setup Tool]: Stored preference for subscriptionId: $subscriptionId"
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Stored preference for subscriptionId: $subscriptionId"
 
 az aks get-credentials --resource-group unitgeniusResourceGroupEastUS --name unitgenius
-echo -e "\n\n[UnitGenius Setup Tool]: Set kubectl context to AKS cluster unitgenius"
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Set kubectl context to AKS cluster unitgenius"
 
 # #---------- Delete Cluster ----------
-echo -e "\n\n[UnitGenius Setup Tool]: Deleting AKS cluster"
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${YELLOW}Deleting AKS cluster...${NC}"
 az aks delete --resource-group unitgeniusResourceGroupEastUS --name unitgenius --yes --no-wait
-echo -e "\n\n[UnitGenius Setup Tool]: Deleted AKS cluster unitgenius"
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}Deleted AKS cluster unitgenius${NC}"
 
 # #---------- Delete RBAC for cluster ----------
-echo -e "\n\n[UnitGenius Setup Tool]: Deleting service principal for AKS cluster"
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${YELLOW}Deleting service principal for AKS cluster...${NC}"
 az ad sp delete --id $appId
-echo -e "\n\n[UnitGenius Setup Tool]: Deleted service principal for AKS cluster"
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}Deleted service principal for AKS cluster${NC}"
 
 # #---------- Delete resource group ----------
-echo -e "\n\n[UnitGenius Setup Tool]: Deleting resource group"
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${YELLOW}Deleting resource group...${NC}"
 az group delete --name unitgeniusResourceGroupEastUS --yes --no-wait
-echo -e "\n\n[UnitGenius Setup Tool]: Deleted resource group unitgeniusResourceGroupEastUS"
-
-#---------- Securely handle preferred secret values ----------
-# !User-> Before continuing, enter your (preffered) values for the following secret(s) in the Github repositories of the project:
-# 	- Repository: unitgenius
-# 	  Secret key: AUTH_DB_CONNECTION_STRING
-# 	  Secret value: <the connection string to database for the auth functionality>
-# 	- Repository: unitgenius, unitgenius-generationservice
-# 	  Secret key: OPENAI_APIKEY
-# 	  Secret value: <your api key for OpenAI.>
-# 	- Repository: ALL repositories
-# 	  Secret key: AZURE_CREDENTIALS
-# 	  Secret value: <enter the following object:>
-# 	- Repository: ALL repositories
-# 	  Secret key: POSTMAN_APIKEY
-# 	  Secret value: <your api key for Postman. Can be set later but is required for integration tests in pipelines to work.>
-# !User-> Press enter when completed.
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}Deleted resource group unitgeniusResourceGroupEastUS${NC}"
 
 
-#---------- Commit and push change to main branch to deploy ----------
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}DONE... Deletion of Azure setup completed successfully${NC}"
+
--- infrastructure/scripts/setup.sh
+++ infrastructure/scripts/setup.sh
@@ -1,89 +1,102 @@
 #!/bin/bash
-#---------- Login to Azure ---------- 
-source preferences.env
-echo -e "\n\n[UnitGenius Setup Tool]: Stored preference for tenantId: $tenantId"
-echo -e "\n[UnitGenius Setup Tool]: Stored preference for subscriptionId: $subscriptionId"
-echo -e "\n[UnitGenius Setup Tool]: Stored preference for appId: $appId"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
 
-# echo -e "\n\n[UnitGenius Setup Tool]: Login to Azure"
+#---------- Login to Azure ---------- 
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${YELLOW}Logging in to Azure...${NC}"
 # login_output=$(az login)
 # tenantId=$(echo $login_output | jq -r '.[0].tenantId')
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}Logged in to Azure...${NC}"
 
-# # echo "tenantId=$tenantId" >> preferences.env
-# echo -e "\n\n[UnitGenius Setup Tool]: Stored preference for tenantId: $tenantId"
-
-# echo -e "\n\n[UnitGenius Setup Tool]: Your Azure subscriptions: "
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Your Azure subscriptions: "
 # az account list --output table
 
-# echo -e "\n[UnitGenius Setup Tool]: Enter your preferred subscriptionId: "
+# echo -e "\n${PURPLE}[UnitGenius Setup Tool]:${NC} Enter your preferred tenantId: "
+# read tenantId
+
+# echo "tenantId=$tenantId" > preferences.env
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Stored preference for tenantId: $tenantId"
+
+# echo -e "\n${PURPLE}[UnitGenius Setup Tool]:${NC} Enter your preferred subscriptionId: "
 # read subscriptionId
 
 # az account set --subscription $subscriptionId
-# echo -e "\n\n[UnitGenius Setup Tool]: Set subscription to $subscriptionId"
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Set subscription to $subscriptionId"
 
-# # echo "subscriptionId=$subscriptionId" >> preferences.env
-# echo -e "\n\n[UnitGenius Setup Tool]: Stored preference for subscriptionId: $subscriptionId"
+# echo "subscriptionId=$subscriptionId" >> preferences.env
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Stored preference for subscriptionId: $subscriptionId"
+
+# az aks get-credentials --resource-group unitgeniusResourceGroupEastUS --name unitgenius
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Set kubectl context to AKS cluster unitgenius"
 
 
 # # #---------- Create resource group ----------
-# echo -e "\n\n[UnitGenius Setup Tool]: Creating resource group"
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${YELLOW}Creating resource group...${NC}"
 # az group create --name unitgeniusResourceGroupEastUS --location eastus
-# echo -e "\n\n[UnitGenius Setup Tool]: Created resource group unitgeniusResourceGroupEastUS"
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}Created resource group unitgeniusResourceGroupEastUS.${NC}"
 
 
 # # #---------- Create RBAC for cluster ----------
-# echo -e "\n\n[UnitGenius Setup Tool]: Creating service principal for AKS cluster"
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${YELLOW}Creating service principal for AKS cluster...${NC}"
 # sp=$(az ad sp create-for-rbac --name unitgeniusAKSClusterServicePrincipal --query "{appId:appId, password:password}" -o tsv)
 # read -r appId clientSecret <<< "$sp"
-# echo -e "\n\n[UnitGenius Setup Tool]: Created service principal for AKS cluster"
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}Created service principal for AKS cluster.${NC}"
 
-# # echo "appId=$appId" >> preferences.env
-# echo -e "\n\n[UnitGenius Setup Tool]: Stored preference for service principal appId: $appId"
+# echo "appId=$appId" >> preferences.env
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Stored preference for service principal appId: $appId"
+# echo "clientSecret=$clientSecret" >> preferences.env
+# # echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Stored preference for service principal clientSecret: $clientSecret"
 
 
 # #---------- Create Cluster ----------
-# echo -e "\n\n[UnitGenius Setup Tool]: Creating AKS cluster"
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${YELLOW}Creating AKS cluster...${NC}"
 # az aks create --resource-group unitgeniusResourceGroupEastUS --name unitgenius --service-principal $appId --client-secret $clientSecret --node-count 1 --generate-ssh-keys
-# echo -e "\n\n[UnitGenius Setup Tool]: Created AKS cluster unitgenius"
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}Created AKS cluster unitgenius.${NC}"
 
 
 # #---------- Set kubectl context ----------
 # az aks get-credentials --resource-group unitgeniusResourceGroupEastUS --name unitgenius
-# echo -e "\n\n[UnitGenius Setup Tool]: Set kubectl context to AKS cluster unitgenius"
+# echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Set kubectl context to AKS cluster unitgenius"
 
 
 #---------- Securely handle preferred secret values ----------
-# !User-> Before continuing, enter your (preffered) values for the following secret(s) in the Github repositories of the project:
-echo -e "\n\n[UnitGenius Setup Tool]: Before continuing, enter your (preffered) values for the following secret(s) in the Github repositories of the project:"
+source preferences.env
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} Before continuing, enter your (preffered) values for the following secret(s) in the Github repositories of the project:"
 echo -e ""
-echo -e "[UnitGenius Setup Tool]:   - Repository: unitgenius"
-echo -e "[UnitGenius Setup Tool]:     Secret key: AUTH_DB_CONNECTION_STRING"
-echo -e "[UnitGenius Setup Tool]:     Secret value: the connection string to database for the auth functionality"
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}   - Repository: unitgenius"
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}     Secret key: AUTH_DB_CONNECTION_STRING"
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}     Secret value: the connection string to database for the auth functionality"
 echo -e ""
-echo -e "[UnitGenius Setup Tool]:   - Repository: unitgenius, unitgenius-generationservice"
-echo -e "[UnitGenius Setup Tool]:     Secret key: OPENAI_APIKEY"
-echo -e "[UnitGenius Setup Tool]:     Secret value: your api key for OpenAI."
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}   - Repository: unitgenius, unitgenius-generationservice"
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}     Secret key: OPENAI_APIKEY"
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}     Secret value: your api key for OpenAI."
 echo -e ""
-echo -e "[UnitGenius Setup Tool]:   - Repository: ALL repositories"
-echo -e "[UnitGenius Setup Tool]:     Secret key: AZURE_CREDENTIALS"
-echo -e "[UnitGenius Setup Tool]:     Secret value: enter the following object: 
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}   - Repository: ALL repositories"
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}     Secret key: AZURE_CREDENTIALS"
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}     Secret value: enter the following object: 
 {
-  '"clientId"': '"$appId"',
-  '"clientSecret"': '"$password"',
-  '"tenantId"': '"$tenantId"',
-  '"subscriptionId"': '"$subscriptionId"'
+  "'clientId'": ""$appId"",
+  "'clientSecret'": ""$clientSecret"",
+  "'tenantId'": ""$tenantId"",
+  "'subscriptionId'": ""$subscriptionId""
 }"
 echo -e ""
-echo -e "[UnitGenius Setup Tool]:   - Repository: ALL repositories"
-echo -e "[UnitGenius Setup Tool]:     Secret key: POSTMAN_APIKEY"
-echo -e "[UnitGenius Setup Tool]:     Secret value: your api key for Postman. Can be set later but is required for integration tests in pipelines to work."
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}   - Repository: ALL repositories"
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}     Secret key: POSTMAN_APIKEY"
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC}     Secret value: your api key for Postman. Can be set later but is required for integration tests in pipelines to work."
 echo -e ""
-echo -e "[UnitGenius Setup Tool]: Press enter when completed:"
+echo -e "${PURPLE}[UnitGenius Setup Tool]:${NC} Press enter when completed:"
 read -r
 
 #---------- Commit and push change to main branch to deploy ----------
-echo -e "\n\n[UnitGenius Setup Tool]: Commit and push change to main branch to deploy"
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${YELLOW}Trying to ommit and push change to main branch to deploy...${NC}"
 git add .
 git commit -m "Setup AKS cluster"
 git push origin main
-echo -e "\n\n[UnitGenius Setup Tool]: Committed and pushed change to main branch to deploy"
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}Done committing and pushing change to main branch to deploy.${NC}"
+
+
+echo -e "\n\n${PURPLE}[UnitGenius Setup Tool]:${NC} ${GREEN}DONE... Setup of Azure environment completed successfully${NC}"
